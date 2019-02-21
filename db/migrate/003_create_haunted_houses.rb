0a5afe3d91f27bdddc3b8e14a04810158c722d7f
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-02-21 18:46:38 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -8,8 +8,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :theme
       t.float :price
       t.boolean :family_friendly
-      t.datetime :open
-      t.datetime :close 
+      t.datetime :opening_date
+      t.datetime :closing_date
       t.text :long_description 
       
       t.timestamps null: false 

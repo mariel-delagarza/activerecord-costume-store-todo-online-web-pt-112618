26fdfff0a2c9284bcbf7102086ac0f776798ec94
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-02-21 18:37:45 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,6 +3,13 @@
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change 
     create_table :costume_stores do |t|
-      
-    
+      t.string :name 
+      t.string :location 
+      t.integer :costume_inventory
+      t.integer :number_of_employees
+      t.boolean :still_in_business 
+      t.datetime :open 
+      t.datetime :close 
+    end 
+  end 
 end 
